aed76272871b9ce705f17485540e9b8602a2f688
Farzaad Goiporia <farzaadg@Farzaads-MacBook-Pro.local>
2024-02-07 20:32:35 -0500

    *Removed Unwanted platforms *Created MainPage UI and Route to HomePage *Started work on HomePageUI

---
+++ FoodMAUI/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodMAUI.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FoodMAUI/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace FoodMAUI.Views;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    async void Button_Pressed(System.Object sender, System.EventArgs e)
+    {
+		await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+    }
+}
+
+
